e322d9a733793dfbc7786697561b98655e10134e
TechQuery <shiy2008@gmail.com>
2025-09-21 02:59:21 +0800

    [add] Toolchain Configuration files for Engineering Automation [fix] some Mock Data bugs in Test cases

--- test/example/model.ts
+++ test/example/model.ts
@@ -1,21 +1,25 @@
 import { HTTPClient } from 'koajax';
-import { ListModel, Filter, NewData, DataObject } from 'mobx-restful';
+import { DataObject, Filter, IDType, ListModel, NewData } from 'mobx-restful';
 
 export abstract class TableModel<T extends DataObject> extends ListModel<T> {
   client = new HTTPClient();
 
-  private mockData: T[] = [];
+  abstract mockData: T[];
 
   async loadPage(pageIndex = 1, pageSize = 10, filter: Filter<T>) {
     const filteredList = this.mockData.filter(item =>
-      Object.entries(filter).every(([key, value]) => item[key as keyof T] === value)
+      Object.entries(filter).every(([key, value]) => item[key as keyof T] === value),
     );
     const pageData = filteredList.slice((pageIndex - 1) * pageSize, pageIndex * pageSize);
 
     return { pageData, totalCount: filteredList.length };
   }
 
-  async updateOne(data: Partial<NewData<T>>, id?: number) {
+  async getOne(id: IDType) {
+    return this.mockData.find(({ [this.indexKey]: itemId }) => itemId === id)!;
+  }
+
+  async updateOne(data: Partial<NewData<T>>, id?: IDType) {
     if (id) {
       const index = this.mockData.findIndex(item => item.id === id);
 
--- test/example/target.ts
+++ test/example/target.ts
@@ -1,3 +1,5 @@
+import { IDType, NewData } from 'mobx-restful';
+
 import { TableModel } from './model';
 
 export interface User extends Record<'name' | 'email', string> {
@@ -10,10 +12,25 @@ export interface Article extends Record<'title' | 'category' | 'content', string
   author: User;
 }
 
+const mockUsers: User[] = [];
+
 export class UserModel extends TableModel<User> {
   baseURI = '/users';
+
+  mockData = mockUsers;
 }
+
+const mockArticles: Article[] = [];
 
 export class ArticleModel extends TableModel<Article> {
   baseURI = '/articles';
+
+  mockData = mockArticles;
+
+  async updateOne(data: Partial<NewData<Article>>, id?: IDType) {
+    const article = await super.updateOne(data, id);
+    const author = await new UserModel().getOne(data.author as IDType);
+
+    return { ...article, author };
+  }
 }
